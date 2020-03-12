2e51fd9700f9a9582b31f8bd52b91a2bbb05b082
Brock T <brockthigpen@yahoo.com>
2020-03-11 23:27:59 -0400

    created HomePage file

---
+++ src/pages/HomePage/index.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import API from "../../utils/API";
+import Container from "../../components/Container";
+import Alert from '../../components/Alert'
+import Table from '../../components/Table'
+
+function HomePage() {
+  const [employeeData, setEmployeeData] = useState([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    API.popEmployees()
+      .then(res => {
+        setEmployeeData(res.data.results);
+      })
+      .catch(err => setError(err));
+  }, []);
+
+  return (
+    <div>
+      <Container style={{ minHeight: "100vh" }}>
+        <h1 className="text-center">Employee Table</h1>
+        <Alert type="danger" style={{ opacity: error ? 1 : 0, marginBottom: 10 }}>
+          {error}
+        </Alert>
+        <Table
+          employeeData={employeeData}
+        />
+      </Container>
+    </div>
+  );
+}
+
+
+export default HomePage;
